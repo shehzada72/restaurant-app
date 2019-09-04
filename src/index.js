1077cf26c7217b5ef86c4531b5c77ed9b643c435
Shehzad Aslam <shehzad.aslam@powersoft19.com>
2019-09-04 15:06:59 +0500

    Start working on restaurants page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import AppRoute from './routes';
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core';
 import {red} from "@material-ui/core/colors";
 
-import "assets/scss/material-kit-react.scss?v=1.7.0";
+import "./assets/scss/material-kit-react.scss?v=1.7.0";
 
 const theme = createMuiTheme({
     palette: {
